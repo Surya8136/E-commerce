77a68169b5bc12cabadc7446b817e3da08d23579
Surya8136 <suryaarya230@gmail.com>
2025-11-16 00:21:09 +0530

    added

--- Tricuisine/menu/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from menu.models import Cuisines, FoodItems, Category
-
-# Register your models here.
-admin.site.register(Cuisines)
-admin.site.register(FoodItems)
-admin.site.register(Category)
--- Tricuisine/menu/context_processor.py
+++
@@ -1,5 +0,0 @@
-from menu.models import Cuisines
-
-def links(request):
-    c=Cuisines.objects.all()
-    return {'links':c}
